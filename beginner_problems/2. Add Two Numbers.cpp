b6eab5348f1eefd9095bd9c6b52df77ccb4abc22
Maheshrmr01 <146946154+Maheshrmr01@users.noreply.github.com>
2025-05-16 01:20:46 +0530

    Create 2. Add Two Numbers.cpp

---
+++ beginner_problems/2. Add Two Numbers.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) 
+    {
+        vector <int> total ;
+        ListNode * l3 = new ListNode;
+        ListNode * tempnode = l3;
+        int carry = 0,sum = 0;
+        while(l1!=nullptr || l2!=nullptr)
+        {
+            sum=0;
+            if (l1!=nullptr)
+            {
+                sum+=l1->val;
+                l1=l1->next;
+            }
+            if (l2!=nullptr)
+            {
+                sum+=l2->val;
+                l2=l2->next;
+            }
+            sum+=carry;
+            carry=sum/10;
+            sum=sum%10;
+            total.push_back(sum);
+        }
+        if (carry==1)
+        {
+            total.push_back(carry);
+        }
+        for(int i=0;i<total.size();i++)
+        {
+
+            tempnode->val = total.at(i);
+            if(i==total.size()-1)
+            {
+                break;
+            }
+            tempnode->next = new ListNode();
+            tempnode = tempnode->next;
+        }
+        return l3;
+    }
+};
